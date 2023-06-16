cdb464d094a2a4d28d398490e1af8b609be7e741
kmarius <5224719+kmarius@users.noreply.github.com>
2023-06-16 11:34:45 +0200

    add test for Filter

---
+++ Google_tests/FilterTest.cpp
@@ -0,0 +1,41 @@
+
+#include <gtest/gtest.h>
+#include "lib/log.h"
+#include "lib/iterators/Filter.h"
+#include "lib/iterators/VectorScan.h"
+#include "lib/iterators/AssertEqual.h"
+
+class FilterTest : public ::testing::Test {
+protected:
+
+    const size_t SEED = 1337;
+
+    void SetUp() override {
+        log_set_quiet(true);
+        log_set_level(LOG_ERROR);
+    }
+
+    void TearDown() override {
+
+    }
+};
+
+TEST_F(FilterTest, EmptyTest) {
+    auto *plan = new AssertEqual(
+            new Filter(
+                    new VectorScan(
+                            {
+                                    {0, 0, {0}},
+                                    {0, 1, {1}},
+                            }
+                    ),
+                    [](const Row *row) { return row->columns[0] % 2 == 0; }),
+            new VectorScan(
+                    {
+                            {0, 0, {0}},
+                    }
+            ));
+    plan->run();
+    ASSERT_TRUE(plan->equal);
+    delete plan;
+}
