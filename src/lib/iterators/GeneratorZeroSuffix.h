d5c20b47be054f82c441d4bcf6ae248d0e241366
kmarius <5224719+kmarius@users.noreply.github.com>
2023-07-06 15:06:58 +0200

    add GeneratorZeroSuffix

---
+++ src/lib/iterators/GeneratorZeroSuffix.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <random>
+#include "IGenerator.h"
+
+class GeneratorZeroSuffix : public IGenerator {
+public:
+    explicit GeneratorZeroSuffix(unsigned long num_rows, int upper, int suffix = 0, unsigned long seed = -1);
+
+    Row *next() override;
+
+    std::vector<Row> rows;
+
+    GeneratorZeroSuffix *clone() const override {
+        return new GeneratorZeroSuffix(num_rows, upper, suffix, seed_);
+    }
+
+private :
+    unsigned long num_rows;
+    int suffix;
+    int upper;
+    std::mt19937 rng;
+    std::uniform_int_distribution<std::mt19937::result_type> dist;
+    Row buf;
+    unsigned long seed_;
+
+};
