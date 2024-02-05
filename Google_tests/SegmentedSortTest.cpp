4ad1d2cecb638e90b796f410772085c1e9e90ba8
kmarius <5224719+kmarius@users.noreply.github.com>
2024-02-05 11:33:41 +0100

    update SegmentedSortTest

--- Google_tests/SegmentedSortTest.cpp
+++ Google_tests/SegmentedSortTest.cpp
@@ -37,8 +37,8 @@ TEST_F(SegmentedSortTest, EmptyTest) {
 }
 
 TEST_F(SegmentedSortTest, BigTest) {
-    unsigned long domain = 128;
-    unsigned long num_rows = 1 << 15;
+    unsigned long domain = 16;
+    unsigned long num_rows = 1 << 12;
 
     uint8_t A[ROW_ARITY] = {0, 1};
     uint8_t B[ROW_ARITY] = {2, 3};
@@ -54,6 +54,77 @@ TEST_F(SegmentedSortTest, BigTest) {
                     EqColumnList(B, sizeof B / sizeof *B),
                     CmpColumnList(CB, sizeof CB / sizeof *CB)),
             CmpColumnList(ACB, sizeof ACB / sizeof *ACB));
+
+    plan.run();
+    assert(plan.isSorted());;
+    assert(plan.getCount() == num_rows);
+}
+
+TEST_F(SegmentedSortTest, BigTest2) {
+    unsigned long domain = 16;
+    unsigned long num_rows = 1 << 12;
+
+    uint8_t A[ROW_ARITY] = {0};
+    uint8_t B[ROW_ARITY] = {1};
+    uint8_t ACB[ROW_ARITY] = {0, 1, 2};
+    uint8_t CB[ROW_ARITY] = {2, 1};
+
+    auto plan = AssertSorted(
+            new SegmentedSort(
+                    new SortPrefix(
+                            new GeneratorWithDomains(num_rows, {domain, domain, domain}, 1337),
+                            sizeof ACB / sizeof *ACB),
+                    EqColumnList(A, sizeof A / sizeof *A),
+                    EqColumnList(B, sizeof B / sizeof *B),
+                    CmpColumnList(CB, sizeof CB / sizeof *CB)),
+            CmpColumnList(ACB, sizeof ACB / sizeof *ACB));
+
+    plan.run();
+    assert(plan.isSorted());;
+    assert(plan.getCount() == num_rows);
+}
+
+TEST_F(SegmentedSortTest, BigTest2OVC) {
+    unsigned long domain = 8;
+    unsigned long num_rows = 1 << 12;
+
+    uint8_t ABC[ROW_ARITY] = {0, 1, 2};
+    uint8_t ACB[ROW_ARITY] = {0, 2, 1};
+
+    auto plan = AssertSorted(
+            new SegmentedSort(
+                    new SortPrefix(
+                            new GeneratorWithDomains(num_rows, {domain, domain, domain}, 1337),
+                            sizeof ABC / sizeof *ABC),
+                    EqOffset(ABC, 3, 1),
+                    EqOffset(ABC, 3, 2),
+                    CmpColumnListCoolOVC(ACB, 3, 2, 1)),
+            CmpColumnListOVC(ACB, sizeof ACB / sizeof *ACB));
+
+    plan.run();
+    assert(plan.isSorted());;
+    assert(plan.getCount() == num_rows);
+}
+
+
+TEST_F(SegmentedSortTest, BigTest3OVC) {
+    unsigned long domain = 16;
+    unsigned long num_rows = 1 << 12;
+
+    uint8_t ABC[ROW_ARITY] = {0, 1, 2, 3, 4, 5};
+    uint8_t ACB[ROW_ARITY] = {0, 3, 4, 1, 2};
+    auto key_length = 6;
+    auto list_length = 2;
+
+    auto plan = AssertSorted(
+            new SegmentedSort(
+                    new SortPrefix(
+                            new GeneratorWithDomains(num_rows, {domain, domain, domain, domain, domain, domain}, 1337),
+                            key_length),
+                    EqOffset(ABC, key_length, list_length),
+                    EqOffset(ABC, key_length, list_length * 2),
+                    CmpColumnListCoolOVC(ACB, key_length, list_length * 2, list_length)),
+            CmpColumnListOVC(ACB, key_length));
 
     plan.run();
     assert(plan.isSorted());;
